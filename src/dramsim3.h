c76a93983acd3f6cd4f4f7ba7eb6ac99a24018b8
Liu Xiaoyi <circuitcoder0@gmail.com>
2024-01-07 17:49:23 +0800

    Install runtime and headers

--- src/dramsim3.h
+++ src/dramsim3.h
@@ -1,9 +1,44 @@
-#ifndef __DRAMSIM3__H
-#define __DRAMSIM3__H
+#ifndef __MEMORY_SYSTEM__H
+#define __MEMORY_SYSTEM__H
 
 #include <functional>
 #include <string>
 
-#include "memory_system.h"
+namespace dramsim3 {
+
+class Config;
+class BaseDRAMSystem;
+
+// This should be the interface class that deals with CPU
+class MemorySystem {
+   public:
+    MemorySystem(const std::string &config_file, const std::string &output_dir,
+                 std::function<void(uint64_t)> read_callback,
+                 std::function<void(uint64_t)> write_callback);
+    ~MemorySystem();
+
+    void ClockTick();
+    void RegisterCallbacks(std::function<void(uint64_t)> read_callback,
+                           std::function<void(uint64_t)> write_callback);
+    double GetTCK() const;
+    int GetBusBits() const;
+    int GetBurstLength() const;
+    int GetQueueSize() const;
+    void PrintStats() const;
+    void ResetStats();
+
+    bool WillAcceptTransaction(uint64_t hex_addr, bool is_write) const;
+    bool AddTransaction(uint64_t hex_addr, bool is_write);
+
+   private:
+    std::unique_ptr<Config> config_;
+    std::unique_ptr<BaseDRAMSystem> dram_system_;
+};
+
+MemorySystem* GetMemorySystem(const std::string &config_file, const std::string &output_dir,
+                 std::function<void(uint64_t)> read_callback,
+                 std::function<void(uint64_t)> write_callback);
+
+}  // namespace dramsim3
 
 #endif
